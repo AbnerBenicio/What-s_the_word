b685cd8a0f213e83174bebcf38bfb5c2c6019ec5
AbnerBenicio <abnerbeniciosilva123@gmail.com>
2024-01-31 21:39:43 -0300

    updating word of game

--- src/data/words.js
+++ src/data/words.js
@@ -1,7 +1,7 @@
 export const wordsList = {
   carro: ["Motor", "Porta", "Capo", "Pneu", "Antena"],
-  fruta: ["Banana", "Uva", "Pêra", "Mamão", "Laranja"],
-  corpo: ["Braço", "Perna", "Cerebro", "Pescoço", "Olhos"],
+  fruta: ["Banana", "Uva", "Pera", "Mamao", "Laranja"],
+  corpo: ["Dedo", "Perna", "Cerebro", "Pescoco", "Olhos"],
   computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
   programação: ["Python", "Java", "Angular", "React"],
   alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"],
